c9f1037c9e3785a8955d0b1c4cc696ffe51d70ce
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-02 18:14:38 -0500

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :hikes, through: :reviews
+    has_secure_password
+    validates :name, :email_address, presence: true 
+    validates :email_address, uniqueness: true
 end
