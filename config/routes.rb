54d0297be0437fd77267ee9c4b86e66ee3bb6032
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-03 16:27:57 -0500

    Updated User routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   resources :islands
   resources :hikes
   resources :reviews
-  resources :users
-  #get 'users/new', to: 'users#new' #, as: 'sign_up'
-  #post 'users/', to: 'users#create'
-  #resources :users, only: [:show]
+  #resources :users
+  get 'sign_up', to: 'users#new' 
+  post 'sign_up', to: 'users#create'
+
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
