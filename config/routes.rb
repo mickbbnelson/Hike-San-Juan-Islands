adc45d94481eeee7b76ea90b4a1484967b7ebf81
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-03 12:54:12 -0500

    user controller methods and user routes temperarily updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :islands
   resources :hikes
   resources :reviews
+  resources :users
+  #get 'users/new', to: 'users#new' #, as: 'sign_up'
+  #post 'users/', to: 'users#create'
   #resources :users, only: [:show]
-  get 'users/sign_up', to: 'users#new', as: 'sign_up'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
