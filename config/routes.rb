70ea2fced7e68f8e68af165d831ab8a33a9bc967
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-03 17:18:06 -0500

    added session routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   #resources :users
   get 'sign_up', to: 'users#new' 
   post 'sign_up', to: 'users#create'
-
   resources :users, only: [:show]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
