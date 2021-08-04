d0f7a751bdb783911635154c3fc4dd012824d487
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-03 21:52:50 -0500

    session login route created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
     end
 
     def create 
+        user = User.find_by_email_address(params[:user][:email_address])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+            byebug
+        else
+            redirect_to login_path
+        end
     end
 
     def destroy
