7c3c3636318ea7d57f79215467011bbe255ac143
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-04 15:30:43 -0500

    auth private method added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,10 +13,17 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
+
     end
 
     def destroy
         session.clear
         redirect_to login_path
+    end
+
+private
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
