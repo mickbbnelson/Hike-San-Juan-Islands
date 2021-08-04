c61c3a69a55458f0241bae484b94f826b3143967
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-04 17:07:06 -0500

    omniauth method created and logout link added.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,13 +13,17 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
-        @user = User.find_or_create_by(uid: auth['uid']) do |u|
-            u.name = auth['info']['name']
-            u.email_address = auth['info']['email']
-            u.password = SecureRandom.hex(12)
+        @user = User.find_or_create_by(uid: auth['uid']) do |user|
+            user.name = auth['info']['name']
+            user.email_address = auth['info']['email']
+            user.password = SecureRandom.hex(12)
         end
+        if @user.valid?
         session[:user_id] = @user.id
         render 'islands/index'
+        else
+        redirect_to login_path
+        end
     end
 
     def destroy
