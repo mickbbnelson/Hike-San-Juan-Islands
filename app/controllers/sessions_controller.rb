d2d921dc06a1887dcf468146ce60c6555b781490
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-03 22:01:30 -0500

    logout destroy session method added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,13 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:user][:password])
             session[:user_id] = user.id
             redirect_to user_path(user)
-            byebug
         else
             redirect_to login_path
         end
     end
 
     def destroy
+        session.clear
+        redirect_to login_path
     end
 end
