6ad5bca8e6372f2c41d15a030ae27dc6aedbbb97
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-04 18:37:15 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper
+
+private
+
+    def not_logged_in_redirect
+        if !logged_in
+            redirect_to login_path
+        end
+    end
 end
