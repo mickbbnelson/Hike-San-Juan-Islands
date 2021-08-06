d9d0731be6e0f52114d5b8173abf4304e57f5ca9
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-06 11:33:02 -0500

    Added review controller shell

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,35 @@
 class ReviewsController < ApplicationController
+    def index
+
+    end
+    
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+private
+
+    def review_params
+        params.require(:post).permit(:content)
+    end
 end
