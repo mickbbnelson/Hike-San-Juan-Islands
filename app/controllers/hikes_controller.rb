818617839f23801f77d859505286aff88fdf4706
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-10 11:40:23 -0500

    github test

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -3,5 +3,6 @@ class HikesController < ApplicationController
     def show
         @island = Island.find_by_id(params[:island_id])
         @hike = @island.hikes.find_by_id(params[:id])
+        
     end
 end
