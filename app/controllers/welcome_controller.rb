076e8a7481b95970ef5bf456cf818478ee549345
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-11 17:28:24 -0500

    Added link to return to island/id/hike/id view from create page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
     def home
-        
+        if current_user
+            session.clear
+            redirect_to root_path
+        end 
     end
 end
