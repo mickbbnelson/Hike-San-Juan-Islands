1520fffdd05516095da9ef2f6f495f013e4f493e
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-01-06 18:54:16 -0600

    Code challenge added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
-    include ApplicationHelper
+    #include ApplicationHelper
+    helper_method :current_user, :logged_in?
 
 private
+def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+end
+
+def logged_in?
+    current_user
+end
 
     def login_redirect
         if !logged_in?
--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,6 @@ class Hike < ApplicationRecord
     belongs_to :island
     has_many :reviews
     has_many :users, through: :reviews 
+
+    scope :most_reviews, -> {joins(:reviews).group(:hike_id).order("COUNT(reviews.id) desc").limit(1)}
 end
