9431174e213197fe11b601ce36d427864d8c47b9
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-04-15 11:45:18 -0600

    Update Omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,10 +2,6 @@ OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], { 
-      :scope => 'email', 'public_profile', :client_options => { :ssl => { 
-          :ca_file => '/usr/lib/ssl/certs/ca-certificates.crt',
-          :ca_path => "/private/etc/ssl/certs"
-      }}
-    }
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], 
+    scope: "identify"
   end 
