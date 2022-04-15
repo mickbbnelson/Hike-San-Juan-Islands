f772c1c4eb72aeb5187ca2afef95d135e4dafa91
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-04-15 10:07:38 -0600

    Test Routes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-# OmniAuth.config.allowed_request_methods = [:post, :get]
-# OmniAuth.config.silence_get_warning = true
+OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
